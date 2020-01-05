c8fcad4133807a51228768fa7ec1e8877bb339ed
Nate Thompson <nate.thompson@digitalglobe.com>
2020-01-04 21:32:06 -0700

    Updated snap distance and added Authy to floating windows

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 7;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=16" };
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	{ "Gimp",                 NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",              NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "jetbrains-pycharm",    NULL,       NULL,       1 << 3,       1,           -1 },
+	{ "Google-chrome",        NULL,       "Authy",    0,            1,           -1 },
 };
 
 /* layout(s) */
